ac2d64b06f3a7e99063b28cd3467daa7b5b28d3b
Naerhy <naerhy@protonmail.com>
2024-05-02 22:34:46 +0200

    Install Prettier and format files

--- projects/mnemosyne/src/index.ts
+++ projects/mnemosyne/src/index.ts
@@ -12,64 +12,65 @@ const UPLOADS_DIR = "static";
 const MAX_SIZE = 209715200;
 
 async function initServer(): Promise<FastifyInstance> {
-	const server = fastify({ bodyLimit: MAX_SIZE});
-	await server.register(cors);
-	server.register(multipart, {
-		limits: {
-			fileSize: MAX_SIZE
-		}
-	});
-	return server;
+  const server = fastify({ bodyLimit: MAX_SIZE });
+  await server.register(cors);
+  server.register(multipart, {
+    limits: {
+      fileSize: MAX_SIZE
+    }
+  });
+  return server;
 }
 
 async function setupRoutes(server: FastifyInstance): Promise<void> {
-	server.post("/files", async (req) => {
-		try {
-			const data = await req.file();
-			// TODO: validate file + mimetype + ...
-			if (!data || !isValidMimeType(data.mimetype)) {
-				throw new Error("Invalid mimetype");
-			}
-			await stream.pipeline(data.file, fs.createWriteStream(path.join(
-				UPLOADS_DIR,
-				data.mimetype.includes("image") ? "photos" : "videos",
-				data.filename
-			)));
-			return data.filename;
-		} catch (err) {
-			console.error(err);
-		}
-	});
+  server.post("/files", async (req) => {
+    try {
+      const data = await req.file();
+      // TODO: validate file + mimetype + ...
+      if (!data || !isValidMimeType(data.mimetype)) {
+        throw new Error("Invalid mimetype");
+      }
+      await stream.pipeline(
+        data.file,
+        fs.createWriteStream(
+          path.join(UPLOADS_DIR, data.mimetype.includes("image") ? "photos" : "videos", data.filename)
+        )
+      );
+      return data.filename;
+    } catch (err) {
+      console.error(err);
+    }
+  });
 }
 
 function isValidMimeType(mimetype: string): boolean {
-	for (const v of validMimeTypes) {
-		if (mimetype === v) {
-			return true;
-		}
-	}
-	return false;
+  for (const v of validMimeTypes) {
+    if (mimetype === v) {
+      return true;
+    }
+  }
+  return false;
 }
 
 const start = async () => {
-	try {
-		const server = await initServer();
-		if (!fs.existsSync(UPLOADS_DIR)) {
-			throw new Error(`directory ${UPLOADS_DIR} does not exist, aborting...`);
-		}
-		for (const dir of ["photos", "videos"]) {
-			const fullPath = path.join(UPLOADS_DIR, dir);
-			if (!fs.existsSync(fullPath)) {
-				fs.mkdirSync(fullPath);
-			}
-		}
-		await setupRoutes(server);
-		await server.listen({ port: PORT });
-		console.log(`Server listening on port ${PORT}`);
-	} catch (err: unknown) {
-		console.error(err instanceof Error ? err.message : "unexpected error");
-		process.exit(1);
-	}
+  try {
+    const server = await initServer();
+    if (!fs.existsSync(UPLOADS_DIR)) {
+      throw new Error(`directory ${UPLOADS_DIR} does not exist, aborting...`);
+    }
+    for (const dir of ["photos", "videos"]) {
+      const fullPath = path.join(UPLOADS_DIR, dir);
+      if (!fs.existsSync(fullPath)) {
+        fs.mkdirSync(fullPath);
+      }
+    }
+    await setupRoutes(server);
+    await server.listen({ port: PORT });
+    console.log(`Server listening on port ${PORT}`);
+  } catch (err: unknown) {
+    console.error(err instanceof Error ? err.message : "unexpected error");
+    process.exit(1);
+  }
 };
 
 start();
