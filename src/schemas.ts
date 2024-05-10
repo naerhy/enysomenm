3c6954fa6e3858e58b642137c68856a35cc4c055
Naerhy <naerhy@protonmail.com>
2024-05-10 10:09:50 +0200

    Add missing validation for people names in .env

--- src/schemas.ts
+++ src/schemas.ts
@@ -2,10 +2,18 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   PORT: z.coerce.number(),
-  JWT_SECRET: z.string(),
-  PASSWORD_ADMIN: z.string(),
-  PASSWORD_USER: z.string(),
-  UPLOADS_DIR: z.string()
+  JWT_SECRET: z.string().min(1),
+  PASSWORD_ADMIN: z.string().min(5),
+  PASSWORD_USER: z.string().min(5),
+  PEOPLE: z.string().transform((value, ctx) => {
+    const people = value.replaceAll(" ", "").split(",");
+    if (people.length === 0 || people.some((p) => p.length === 0)) {
+      ctx.addIssue({ code: z.ZodIssueCode.custom, message: "People is either empty or contains invalid names" });
+      return z.NEVER;
+    }
+    return people;
+  }),
+  UPLOADS_DIR: z.string().min(1)
 });
 
 export type Env = z.infer<typeof envSchema>;
